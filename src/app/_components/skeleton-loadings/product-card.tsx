45efca2b822521332275a86d18fbdf9b9596bcd8
Emad <emadmohagheghi1383@gmail.com>
2025-05-07 22:09:57 +0330

    product-card loading

---
+++ src/app/_components/skeleton-loadings/product-card.tsx
@@ -0,0 +1,8 @@
+export const ProductCardLoading = () => (
+  <div className="flex flex-col items-center w-[170px] sm:w-[200px] md:w-[230px] h-[300px] bg-[#fff] px-3 py-4 gap-2 rounded-lg">
+    <div className="size-[200px] bg-black/15 rounded-md animate-pulse"></div>
+    <div className="w-full h-4 bg-black/15 rounded-md animate-pulse"></div>
+    <div className="w-full h-4 bg-black/15 rounded-md animate-pulse"></div>
+    <div className="w-full h-8 bg-black/15 rounded-md animate-pulse"></div>
+  </div>
+);
